e75adc32968df76dce33021da35144a5210b286b
NathanaelGT <gordontanu2004@gmail.com>
2024-11-07 16:29:17 +0800

    feat: Implement bare minimum store CRUD

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import (
-    hello,
-)
+from app.api.v1.endpoints import stores
 
 api_router = APIRouter()
-api_router.include_router(hello.router, prefix="/hello", tags=["hello"])
+api_router.include_router(stores.router, prefix="/stores", tags=["store"])
