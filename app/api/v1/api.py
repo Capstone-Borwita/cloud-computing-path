9bc4dd9c1ebe953f0f1c123f7ebd92248ca31e2c
NathanaelGT <gordontanu2004@gmail.com>
2024-11-13 13:49:32 +0800

    chore: Disable `users` endpoint

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import stores
-from app.api.v1.endpoints import users
+
+# from app.api.v1.endpoints import users
 from app.api.v1.endpoints import auth
 
 api_router = APIRouter()
-api_router.include_router(stores.router, prefix="/stores", tags=["store"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-
+api_router.include_router(stores.router, prefix="/stores", tags=["store"])
+# api_router.include_router(users.router, prefix="/users", tags=["users"])
