f8f11d5366ef4f71eacb67e3a86c55599a899151
NathanaelGT <gordontanu2004@gmail.com>
2024-11-21 11:14:07 +0800

    fix: Prevent default avatars from being deleted

--- app/utils/images/avatar.py
+++ app/utils/images/avatar.py
@@ -6,8 +6,11 @@ USER_AVATAR_PATH = Path("images/user-content/avatar")
 USER_AVATAR_PATH.mkdir(parents=True, exist_ok=True)
 
 DEFAULT_USER_AVATAR_PATH = Path("images/default/avatar")
-default_user_avatars = listdir(DEFAULT_USER_AVATAR_PATH)
+default_user_avatars = [
+    str(DEFAULT_USER_AVATAR_PATH / default_user_avatar)
+    for default_user_avatar in listdir(DEFAULT_USER_AVATAR_PATH)
+]
 
 
 def random_user_avatar() -> str:
-    return str(DEFAULT_USER_AVATAR_PATH / random.choice(default_user_avatars))
+    return random.choice(default_user_avatars)
